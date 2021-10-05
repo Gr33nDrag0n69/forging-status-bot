f29edbd326ebbaa5991f807315aa7b4878a63ed0
Gr33nDrag0n <Gr33nDrag0n69@users.noreply.github.com>
2021-10-05 00:35:38 -0400

    Update logmonitor.sh

--- logmonitor.sh
+++ logmonitor.sh
@@ -5,18 +5,16 @@
 LiskLogFile="$HOME/.lisk/lisk-core/logs/lisk.log"
 
 tail -Fn0 "$LiskLogFile" | \
-while read Line ; do
-    #echo "$Line" | jq '.'
+while read -r Line ; do
     if [[ $Line == *"Forged new block"* ]]; then
-        sleep 9
-        Delegate=$( echo "$Line" | jq -r '.meta.generatorAddress' )
+        sleep 1
+        GeneratorAddress=$( echo "$Line" | jq -r '.meta.generatorAddress' )
         Height=$( echo "$Line" | jq '.meta.height' )
 
         echo '--------------------------------------------------------------------------------'
-        echo "$Height $Delegate Forged New Block"
-        echo "$Line" | jq '.'
+        echo "$Height $GeneratorAddress Forged New Block"
         echo '--------------------------------------------------------------------------------'
 
-        #"$HOME/forger-sync/sene-message.sh" -d "$Delegate" -h "$Height"
+        "$HOME/forging-status-bot/send-message.sh" -a "$GeneratorAddress"
     fi
 done
