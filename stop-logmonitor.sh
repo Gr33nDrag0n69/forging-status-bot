61e36032c3e912bebcf0d21746dd3bf55e8339da
Gr33nDrag0n <Gr33nDrag0n69@users.noreply.github.com>
2021-10-16 00:10:36 -0400

    Update stop-logmonitor.sh

--- stop-logmonitor.sh
+++ stop-logmonitor.sh
@@ -4,7 +4,7 @@
 # Author  :   Gr33nDrag0n
 # Version :   1.0.0
 # GitHub  :   https://github.com/Gr33nDrag0n69/forging-status-bot
-# History :   2021/10/04 - v1.0.0
+# History :   2021/10/15 - v1.0.0
 ###############################################################################
 
-ps -ef | grep "$HOME/forging-status-bot/logmonitor.sh" | grep -v grep | awk '{print $2}' | xargs -r kill -9 &>/dev/null
+pgrep -f 'forging-status-bot/logmonitor.sh' | xargs -r kill -9 &>/dev/null
