a9b09bb84534638d7d273940e881fed6075b4e55
Gr33nDrag0n <Gr33nDrag0n69@users.noreply.github.com>
2021-10-16 00:10:04 -0400

    Update keepalive.sh

--- keepalive.sh
+++ keepalive.sh
@@ -4,10 +4,11 @@
 # Author  :   Gr33nDrag0n
 # Version :   1.0.0
 # GitHub  :   https://github.com/Gr33nDrag0n69/forging-status-bot
-# History :   2021/10/04 - v1.0.0
+# History :   2021/10/15 - v1.0.0
 ###############################################################################
 
-ProcessCount=$( ps -ef | grep 'forging-status-bot/logmonitor.sh' | grep -v grep | wc -l )
+ProcessCount=$( pgrep -f 'forging-status-bot/logmonitor.sh' -c )
+
 if [ "$ProcessCount" -lt 1 ]
 then
     "$HOME/forging-status-bot/start-logmonitor.sh"
